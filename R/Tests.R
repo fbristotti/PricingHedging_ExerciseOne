088b9ada37175aa0baf86e2cf6d6abae06ea5296
fbristotti <fbristotti@gmail.com>
2016-04-17 14:56:11 -0300

    primeira versão do brownian bridge

--- R/Tests.R
+++ R/Tests.R
@@ -1,54 +1,60 @@
 brownm <- function(n, T,x0=0){
 	sigma = sqrt(T / n)
-	sample = c(x0, rnorm(n-1,0,sigma))
+	sample = c(x0, sigma * rnorm(n-1,0,1))
 	return(cumsum(sample))
 }
 
-
-
-brownbridge.recursive <- function(inf,sup,sample){
+brownbridge.recursive <- function(inf,sup,data){
 	if(inf+1>sup){
-		return(sample)
+		return(data)
 	}
 	
 	mid = floor((inf+sup)/2)
 	if(mid==inf | mid==sup){
-		return(sample)
+		return(data)
 	}
 
-	inf_value = sample[inf]
-	sup_value = sample[sup]
+	infval = data$sample[inf]
+	supval = data$sample[sup]
 
-	mean = ((sup-mid)*sample[inf] + (mid-inf)*sample[sup])/(sup-inf)
-	variance = sqrt(((mid-inf)*(sup-mid))/(sup-inf))
+	mean = ((sup-mid)*infval + (mid-inf)*supval)/(sup-inf)
+	data$mean[mid] = mean
+	sigma = (((sup-mid)*(mid-inf))/(sup-inf)) / length(data$sample)
 
-	sample[mid] = rnorm(1,mean,variance)
-	sample = brownbrecursive(inf,mid,sample)
-	sample = brownbrecursive(mid,sup,sample)
-	return(sample)
+	data$sigma[mid] = sigma
+
+	data$sample[mid] = mean + sqrt(sigma) * rnorm(1,0,1)
+	data = brownbridge.recursive(inf,mid,data)
+	data = brownbridge.recursive(mid,sup,data)
+	return(data)
 }
 
 brownbridge <- function(n,T,x0=0){
 
-	mean = 0
 	sigma = c(rep(0,n-1),1)
 	mean = rep(0,n)
-	sample = c(x0,rep(0,n-2),rnorm(1,mean,sigma))
+	sample = c(x0,rep(0,n-2),rnorm(1,0,1))
+	data = list("sample"=sample,"mean"=mean,"sigma"=sigma)
+
 	ini = 1
 	fim = n
 	
-	sample =  brownbridge.recursive(1,length(sample),sample)
+	data =  brownbridge.recursive(1,length(sample),data)
 	
-	return(sample)
+	return(data)
 }
 
 mytest <- function(n){
 	return(list(rep(0,n),rep(1,n))
 }
 
-plot(brownbridge(100,1),type="l",col="blue",ylab="path")
-lines(brownm(100,1),col="red")
-
+n = 1000
+data=brownbridge(n,1)
+plot(data$sample,type="l",col="blue",ylab="path")
+lines(brownm(n,1),col="red")
+data$sample
+data$mean
+data$sigma
 
 r=0
 q=0
